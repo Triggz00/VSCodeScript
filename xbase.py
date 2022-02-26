8bfd37df8e00d162e18e221ba7cbeb9deeaf2f75
Triggz00 <100037171+Triggz00@users.noreply.github.com>
2022-02-26 21:56:32 +0000

    Add files via upload

---
+++ xbase.py
@@ -0,0 +1,23 @@
+import os 
+print("Welcome to my python script checkout my github at https://github.com/Triggz00 !")
+
+print("Enjoy!!!")
+
+sub = input("Enter your email here please 😮‍💨😮‍💨😮‍💨")
+Advert = input("Advert time only jokeing continue (y/n):")
+
+print("Please enter your Banking info here people ")
+print("Wait Where am i scammer 👮indian police shi wtf they gonna do ")
+print("क्या मैं आपको दूर जाने के लिए भुगतान कर सकता हूं? (Google Translate Link https://www.translate.google.com)")
+
+police =input("Are you scamming??????")
+print("No sir why would i ")
+
+print("You have a lambo outside bro")
+lambo =input("Hide the lambo (y/n)")
+print("Oh SHi")
+dead =input("Are you Dead???")
+print("Dead BOI")
+
+python =input("Did you like this python script? (Y/N)")
+print("Good answer ",python)
